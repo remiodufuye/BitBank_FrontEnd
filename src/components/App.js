cb1620fd407eda5033d01f667eab8dc7d76b2a32
github username <github email address>
2020-01-30 15:33:52 -0500

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route , Switch } from "react-router-dom"
 import '../App.css';
 import HeaderComponent from './HeaderComponent' 
 import AllCurrenciesContainer from './AllCurrenciesContainer'
+// import MainContainer from './MainContainer' 
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
       <div className="App">
         <HeaderComponent />
         <Switch>
-           <Route path ="/" component={AllCurrenciesContainer} />
+           <Route path ="/" component={AllCurrenciesContainer}  />
        </Switch>
         
       </div>
---
+++ src/components/MainContainer.js
@@ -0,0 +1,17 @@
+
+import React , {Component} from 'react'
+import AllCurrenciesContainer from './AllCurrenciesContainer'
+import PortfolioContainer from './PortfolioContainer'
+import WatchListContainer from './WatchListContainer'
+
+const MainContainer = () => {
+    return(
+        <div>
+            
+
+        </div>
+    )
+}
+
+export default MainContainer 
+
