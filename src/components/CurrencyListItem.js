ee1d5232ccb7e4a81feddf0ca32ae7bc3c3b5b2a
github username <github email address>
2020-01-31 11:57:34 -0500

    changes made

--- src/components/CurrencyListItem.js
+++ src/components/CurrencyListItem.js
@@ -4,21 +4,13 @@ import { Image as ImageComponent, Item } from 'semantic-ui-react'
 
 // const paragraph = <ImageComponent src='/images/wireframe/short-paragraph.png' />
 
-const CurrencyListItem = props => {
-  return (<div>
-     <Item.Group link>
-    <Item>
-      <Item.Image size='tiny' src='/images/avatar/large/stevie.jpg' />
-
-      <Item.Content>
-        <Item.Header>{props.currency.name}</Item.Header>
-        <Item.Description>{""}</Item.Description>
-      </Item.Content>
-    </Item>
-  </Item.Group>
-  </div>
-  )
-  }
+const CurrencyListItem = props =>  (
+     <div>
+      <h2> {props.currency.website} </h2>
+      <h2>{props.currency.coin_id}</h2>
+      <h2>{props.currency.name}</h2>
+     </div>
+  ) ;
 
 export default CurrencyListItem 
 
@@ -36,3 +28,20 @@ export default CurrencyListItem
 //   )
 //   }
 
+
+// with Semantic 
+// const CurrencyListItem = props => {
+//   return (<div>
+//      <Item.Group link>
+//     <Item>
+//       <Item.Image size='tiny' src='/images/avatar/large/stevie.jpg' />
+
+//       <Item.Content>
+//         <Item.Header>{props.currency.name}</Item.Header>
+//         <Item.Description>{""}</Item.Description>
+//       </Item.Content>
+//     </Item>
+//   </Item.Group>
+//   </div>
+//   )
+//   }
