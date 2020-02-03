a149f58695beb6422a39a2c5d44a1fa2df05d57e
github username <github email address>
2020-02-03 15:20:13 -0500

    changes made

--- src/currencydetail/currencyDetail.js
+++ src/currencydetail/currencyDetail.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import {Link , withRouter} from 'react-router-dom' 
 import {connect} from 'react-redux'
 import {Segment , Button , Label , Container  , Card , Image} from 'semantic-ui-react'
+import NumberFormat from 'react-number-format'
 
 
 class CurrencyDetail extends Component {
@@ -10,33 +11,43 @@ class CurrencyDetail extends Component {
     // console.log("CurrencyDetail props", this.props) 
     return !this.props.currency ? null : (
     <Container>
-        
-        {/* Currency Details Here <br />
-       <CurrencyDetailHeader currency={this.props.currency}/> */}
-    
+
       <Card>
       <Card.Content >
         <Image floated='right' size='mini'src={this.props.currency.logo}/>
         <Card.Header> {this.props.currency.name}</Card.Header>
         <Card.Meta><strong>Symbol: {this.props.currency.symbol}</strong></Card.Meta>
-        <Card.Meta><strong>Price: ${this.props.currency.price}</strong></Card.Meta>
-        <Card.Meta><strong>Max Supply: {this.props.currency.max_supply}</strong></Card.Meta>
-        <Card.Meta><strong>Market: {this.props.currency.market_cap}</strong></Card.Meta>
+         <Card.Meta>
+          <strong>Price:
+          <NumberFormat value={this.props.currency.price} displayType={'text'} thousandSeparator={true} prefix={' $'} /></strong>
+         </Card.Meta>
+         <Card.Meta>
+          <strong>Max Supply:
+          <NumberFormat value={this.props.currency.max_supply} displayType={'text'} thousandSeparator={true} /></strong>
+         </Card.Meta>
+         <Card.Meta>
+          <strong>Market Cap:
+          <NumberFormat value={this.props.currency.market_cap} displayType={'text'} thousandSeparator={true} /></strong>
+         </Card.Meta>
       </Card.Content> 
       </Card>  
 
+      {/* For Description */} 
       <Card>
         <Container>
           <p> <Card.Meta><strong>{this.props.currency.description}</strong></Card.Meta></p>
         </Container>
       </Card> 
-
+      
+      {/* Button  */}
       <Segment>
        <Button content='Buy' color= "green" icon='money bill alternate outline' labelPosition='left' />
        <Button content='Watch' color ="blue" icon='eye' labelPosition='right' />
        </Segment> 
 
     </Container> 
+
+    
     ) ; 
   }
 } 
