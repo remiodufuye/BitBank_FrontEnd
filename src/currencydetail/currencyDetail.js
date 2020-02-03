790f928997bde25f18810ad017112053f509b58c
github username <github email address>
2020-02-03 15:03:49 -0500

    changes made

--- src/currencydetail/currencyDetail.js
+++ src/currencydetail/currencyDetail.js
@@ -2,17 +2,45 @@
 import React, {Component} from 'react'
 import {Link , withRouter} from 'react-router-dom' 
 import {connect} from 'react-redux'
-import currencyDetailHeader from './currencyDetailHeader' 
+import {Segment , Button , Label , Container  , Card , Image} from 'semantic-ui-react'
 
 
 class CurrencyDetail extends Component {
   render() {
-    console.log("CurrencyDetail props", this.props) 
+    // console.log("CurrencyDetail props", this.props) 
     return !this.props.currency ? null : (
-     <div> Currency Details Here </div>
+    <Container>
+        
+        {/* Currency Details Here <br />
+       <CurrencyDetailHeader currency={this.props.currency}/> */}
+    
+      <Card>
+      <Card.Content >
+        <Image floated='right' size='mini'src={this.props.currency.logo}/>
+        <Card.Header> {this.props.currency.name}</Card.Header>
+        <Card.Meta><strong>Symbol: {this.props.currency.symbol}</strong></Card.Meta>
+        <Card.Meta><strong>Price: ${this.props.currency.price}</strong></Card.Meta>
+        <Card.Meta><strong>Max Supply: {this.props.currency.max_supply}</strong></Card.Meta>
+        <Card.Meta><strong>Market: {this.props.currency.market_cap}</strong></Card.Meta>
+      </Card.Content> 
+      </Card>  
+
+      <Card>
+        <Container>
+          <p> <Card.Meta><strong>{this.props.currency.description}</strong></Card.Meta></p>
+        </Container>
+      </Card> 
+
+      <Segment>
+       <Button content='Buy' color= "green" icon='money bill alternate outline' labelPosition='left' />
+       <Button content='Watch' color ="blue" icon='eye' labelPosition='right' />
+       </Segment> 
+
+    </Container> 
     ) ; 
   }
 } 
+
 
 const mapStateToProps = (store, ownProps) => ({
   currency: store.currencies.find(
--- src/currencydetail/currencyDetailHeader.js
+++ src/currencydetail/currencyDetailHeader.js
@@ -3,19 +3,37 @@
 import React from 'react'
 import { Card , Image } from 'semantic-ui-react'
 
-const currencyDetailHeader = props =>  (
+const CurrencyDetailHeader = props =>  (
       <Card>
       <Card.Content>
-        <Image
-          floated='right'
-          size='mini'
-          src={props.currency.logo}
-        />
-        <Card.Header>{props.currency.name}</Card.Header>
-        <Card.Meta><strong>{props.currency.symbol}</strong></Card.Meta>
-        <Card.Meta><strong>${props.currency.price}</strong></Card.Meta>
-      </Card.Content>
-    </Card>
-  ) ;
+        <Image floated='right' size='mini'src={props.currency.logo}/>
+        <Card.Header> {props.currency.name}</Card.Header>
+        <Card.Meta><strong>Symbol: {props.currency.symbol}</strong></Card.Meta>
+        <Card.Meta><strong>Price: ${props.currency.price}</strong></Card.Meta>
+        <Card.Meta><strong>Max Supply: {props.currency.max_supply}</strong></Card.Meta>
+        <Card.Meta><strong>Market: {props.currency.market_cap}</strong></Card.Meta>
+      </Card.Content> 
+    </Card> 
+  ) 
 
-  export default currencyDetailHeader 
+  export default CurrencyDetailHeader 
+
+
+
+//     <Card>
+//     <Card.Content>
+//       <Image
+//         floated='right'
+//         size='mini'
+//         src={props.currency.logo}
+//       />
+//       <Card.Header> {props.currency.name}</Card.Header>
+//       <Card.Meta><strong>Symbol: {props.currency.symbol}</strong></Card.Meta>
+//       <Card.Meta><strong>Price: ${props.currency.price}</strong></Card.Meta>
+//       <Card.Meta><strong>Max Supply: {props.currency.max_supply}</strong></Card.Meta>
+//       <Card.Meta><strong>Market: {props.currency.market_cap}</strong></Card.Meta>
+//     </Card.Content> 
+//   </Card> 
+// 
+
+
