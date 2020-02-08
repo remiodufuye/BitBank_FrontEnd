ddc4a9b7353cfddba9bdc999150f820a0a190db5
github username <github email address>
2020-02-07 20:15:22 -0500

    fixed scroll on currency details

--- src/components/CurrencyList.js
+++ src/components/CurrencyList.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import CurrencyListItem from './CurrencyListItem'
 import {connect} from 'react-redux'
-import {Grid, Card} from 'semantic-ui-react'
+import {Grid, Card , Segment} from 'semantic-ui-react'
 import SubHeader from './SubHeader'
 import Searchbar from '../components/SearchBar'
 
@@ -20,13 +20,15 @@ const CurrencyList = props => props.loading ? <p>Loading Currencies...</p> : (
                <br /> 
                <br /> 
                 <Grid columns="three" divided>
-                <Card.Group itemsPerRow={3}>
+                <Segment style={{overflow: 'auto', maxHeight: 350 }} >
+                <Card.Group itemsPerRow={5}>
                 { props.currencies.filter(currency => currency.name.toLowerCase().includes(props.searchText.toLowerCase())).map(currency => (
                     < CurrencyListItem
                     key ={currency.id}
                     currency={currency}/> 
                 ))}
                 </Card.Group>
+                </Segment>
                 </Grid>
             </div>
         </div> 
