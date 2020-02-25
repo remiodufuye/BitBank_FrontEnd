881faad285641c783635dee0191ef97aae9c45a6
github username <github email address>
2020-02-25 15:13:12 -0500

    changes made

--- src/components/AmountInput.js
+++ src/components/AmountInput.js
@@ -1,9 +1,5 @@
 
 import React from 'react'
-// import {connect} from 'react-redux' 
-// import {inputAmount} from '../redux/actionCreators' 
-
-
 
 const AmountInput = props => (
   <div class="ui icon input">
@@ -17,21 +13,4 @@ const AmountInput = props => (
 
 
 export default AmountInput 
-
-
-
-// const AmountInput = props => (
-//   <div class="ui icon input">
-//   <input type="number" 
-//   value={props.value}
-//   onChange={(e) => props.inputAmount(e.target.value)}/>
-//   </div>
-// )
-  
-//  const mapStateToProps = (store) => ({
-//     value: store.amount
-//   })
-  
-
-// export default connect(mapStateToProps,{inputAmount})(AmountInput) 
 
