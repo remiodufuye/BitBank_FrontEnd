ebb7308b7c6b7d2943035a8499b5ba946d41eda2
github username <github email address>
2020-07-27 12:43:28 -0400

    changes made

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom"
-import './index.css';
-import App from './container/App';
-import {Provider} from 'react-redux'
-import store from './redux/store'
-import 'semantic-ui-css/semantic.min.css'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./container/App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter> , 
-document.getElementById('root'));
-
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
